bb69ecf2817ccea3266d2a45e235bfa7b6594ed8
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-10 11:40:23 +0200

    correction

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>partie 3 exercice 3</title>
 </head>
 <body>
 <?php
         $nb1 = 100;
-        $nb2 = 24;
-        $somme = NULL;
+        $nb2 = rand(1, 100);
         while ($nb1 >= 10) {
-            $somme = $nb1 * $nb2;
-            echo '<p>' . $somme . '</p>';
+            $nb1 = $nb1 * $nb2;
+            echo '<p>' . $nb1 . '</p>';
             $nb1--;
         }
     ?>
